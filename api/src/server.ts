f34fae50e4f1acb6bcb17337cd1a4d9e05721378
Diovane Alves <alvdiovane@gmail.com>
2024-01-17 20:43:07 -0300

    feat(back-end): integrate Swagger for API documentation

--- api/src/server.ts
+++ api/src/server.ts
@@ -1,6 +1,8 @@
+import swagger from "@elysiajs/swagger";
 import { Elysia } from "elysia";
 import { questionsRoutes } from "./routes/questions.route";
 
 new Elysia()
+  .use(swagger())
   .use(questionsRoutes)
   .listen(3001, () => console.log("Server started at port 3001"));
