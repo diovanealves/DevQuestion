49b876807f8f232ca18242b70dbb112d255f5042
Diovane Alves <alvdiovane@gmail.com>
2024-02-03 15:01:19 -0300

    chore(front-end): start front-end project

---
+++ web/src/app/layout.tsx
@@ -0,0 +1,23 @@
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import "./globals.css";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+  title: "DevQuestion",
+  description:
+    "Explore um ambiente de perguntas e respostas anônimas, aonde você pode fazer perguntas e responder sem revelar a sua identidade.",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body className={inter.className}>{children}</body>
+    </html>
+  );
+}
