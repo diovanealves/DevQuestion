0d897cff3b5e16fe01d48d29d283307a36a6982e
Diovane Alves <alvdiovane@gmail.com>
2024-02-07 22:30:24 -0300

    refactor(front-end): Improve responsiveness of home page

--- web/src/app/(Home)/page.tsx
+++ web/src/app/(Home)/page.tsx
@@ -3,11 +3,11 @@ import CategoryCard from "./CategoryCard";
 
 export default function Category() {
   return (
-    <div className="flex h-screen flex-col items-center justify-center">
+    <div className="flex h-screen flex-col items-center justify-center p-2">
       <h1 className="mb-14 text-3xl font-bold">DevQuestion</h1>
 
       <h1 className="font-mediu mb-3 text-lg">Escolha uma categoria abaixo!</h1>
-      <div className="grid grid-cols-4 gap-10">
+      <div className="grid grid-cols-2 gap-2 md:grid-cols-4  md:gap-8">
         {mockCategories.map((category) => (
           <CategoryCard key={category.id} variant={category.name}>
             <category.icon size={38} />
