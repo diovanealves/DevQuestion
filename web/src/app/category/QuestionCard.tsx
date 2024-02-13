3d92b3aead3b317beb05b78db90f67e4973b83fd
Diovane Alves <alvdiovane@gmail.com>
2024-02-13 16:25:00 -0300

    feat(front-end): Add Question Card to Category Screen

--- web/src/app/category/QuestionCard.tsx
+++ web/src/app/category/QuestionCard.tsx
@@ -1,5 +1,20 @@
 import { Question } from "@/types/question.types";
+import { Forward } from "lucide-react";
+import Link from "next/link";
 
 export default function QuestionCard(props: Question) {
-  return <div>{props.title}</div>;
+  return (
+    <div className="mt-6 flex items-center justify-between rounded-lg border-2 border-gray-500 px-5 py-4">
+      <div>
+        <h1 className="text-xl font-bold leading-tight">{props.title}</h1>
+        <p className="mt-2 text-sm opacity-85">{props.description}</p>
+      </div>
+      <Link
+        href="#"
+        className="hover:text-slate-5000 rounded-lg border-2 border-slate-500 bg-slate-500 p-2 text-white  transition-colors hover:border-slate-500 hover:bg-white hover:text-slate-500"
+      >
+        <Forward size={36} />
+      </Link>
+    </div>
+  );
 }
