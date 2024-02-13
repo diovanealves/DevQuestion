f0c183329da392f53311ba5a1db35b15e88b1b42
Diovane Alves <alvdiovane@gmail.com>
2024-02-13 16:25:47 -0300

    refactor(front-end): Enhance Category page code

--- web/src/app/category/page.tsx
+++ web/src/app/category/page.tsx
@@ -1,23 +1,27 @@
 "use client";
 
+import Loading from "@/components/Loading";
 import { useQuestionsByCategory } from "@/hooks/useQuestions";
 import { Question } from "@/types/question.types";
 import { redirect, useSearchParams } from "next/navigation";
 import QuestionCard from "./QuestionCard";
 
 export default function Category() {
-  const category = useSearchParams().get("category");
+  const category = useSearchParams().get("category") as string;
 
-  if (!category) {
+  const isValid = ["Front-End", "Back-End", "Mobile", "DevOps"].includes(
+    category,
+  );
+  if (!isValid) {
     redirect("/");
   }
 
   const { data, isLoading, isError } = useQuestionsByCategory(category);
 
-  if (isLoading) return <div>...Carregando</div>;
+  if (isLoading) return <Loading />;
 
   return (
-    <div className="pl-4 pt-6">
+    <div className="px-4 pt-6">
       <h1 className="text-2xl font-bold leading-relaxed">
         Perguntas sobre {category}
       </h1>
